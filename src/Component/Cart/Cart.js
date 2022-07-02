eff35f97bad3bc5585965cfaa0fe2dbe80c32c14
saifur1681 <moonly1681@gmail.com>
2022-07-02 20:50:52 +0600

    cmt

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -4,15 +4,21 @@ import './Cart.css';
 const Cart = (props) => {
     const { cart } = props;
     console.log(cart)
-    // total price.....
+    // total price......
     let total = 0;
     let totalQuantity = 0;
+    // let seller = " ";
     for (const product of cart) {
         if (!product.quantity) {
             product.quantity = 1;
         }
         totalQuantity = totalQuantity + product.quantity;
         total = total + product.price * product.quantity;
+        // seller = product.seller;
+        // const ul = document.getElementsByClassName('ul-div');
+        // const li = document.createElement('li');
+        // li.innerText = seller;
+        // ul.appendChild(li);
     };
     // tax...
     let tax = total / 10;
@@ -25,6 +31,9 @@ const Cart = (props) => {
         <div>
             <div className="cart-header">
                 <h2>Order Summary</h2>
+                {/* <div>
+                    <ul className="ul-div" >Seller: {seller}</ul>
+                </div> */}
                 <h3>Ordered Item: {totalQuantity}</h3>
             </div>
 
